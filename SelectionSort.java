3787a7903c935b2d73c02f66624d252b33e3733d
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-07-15 19:51:40 +0900

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -9,12 +9,12 @@ public class SelectionSort
 		int n = arr.length;
 
 		// 정렬되지 않은 하위 배열의 경계를 하나씩 이동
-		// n-1인 이유 : 정렬되지 않은 하위 배열의 경계를 이동하면서
-		// 맨 마지막 요소까지 비교하지 않아도 되기 때문에 n-1까지만 반복
+		// n-1인 이유 : j가 n번까지 다음 인덱스로 있어야 때문
 		for (int i = 0; i < n-1; i++)
 		{
 			// 정렬되지 않은 배열에서 최소 요소 찾기
 			// mid_idx는 0번 인덱스 부터 차례로 최소 요소를 찾기 위함
+			// inner for문의 j는 다음인덱스를 뜻함, i를 비교하기 위해 쓰여짐
 			int min_idx = i;
 			for (int j = i+1; j < n; j++)
 				if (arr[j] < arr[min_idx])
