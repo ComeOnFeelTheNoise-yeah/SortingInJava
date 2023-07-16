034e79870efb05f311ebcb227145f3ce3ae4b85e
ComeOnFeelTheNoise-yeah <nick.washswat@gmail.com>
2023-07-16 23:25:51 +0900

    Update ShellSort.java

--- ShellSort.java
+++ ShellSort.java
@@ -21,27 +21,25 @@ class ShellSort
 		// (추가)간격은 전체길이 나누기 2; 간격이 영보다 클때까지; 간격을 2싹 나눠가면서
 		for (int gap = n/2; gap > 0; gap /= 2)
 		{
-			// 이 간격 크기에 대해 간격 삽입 정렬을 수행합니다.
+			// 이 간격 크기에 대해 간격 삽입 정렬을 수행
 			// 첫 번째 간격 요소 a[0..gap-1]은
-			// 이미 간격이 있는 순서로, 하나 이상의 요소를 계속 추가합니다.
+			// 이미 간격이 있는 순서로, 하나 이상의 요소를 계속 추가
 			// 전체 배열이 갭 정렬될 때까지
 			//(추가) i++을 해도 되지만 통일성있게
 			for (int i = gap; i < n; i += 1) 
 			{
-				// 간격이 있는 요소에 array[i]를 추가합니다.  
-				// 임시로 array[i]를 저장하고 다음에 구멍을 만듭니다.
-				// 위치 i
+				// 간격이 있는 요소에 array[i]를 추가
+				// 임시로 temp에 a[i]를 저장하고 인덱스 i가 있는 값을 빈상태로 만듬
 				int temp = array[i];
 
-				// 이전 갭 정렬 요소를 다음까지 이동합니다.
-				// a[i]의 올바른 위치를 찾았습니다.
-				// j를 밖으로 뺸 이유 : outer for문 에서 쓰기 위함
+				// 이전 갭 정렬 요소를 다음까지 이동
+				// a[i]의 올바른 위치를 찾음
+				// j를 밖으로 뺸 이유 : outer loop(for) 에서 쓰기 위함
 				int j;
 				for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 					array[j] = array[j - gap];
 
-				// temp(원본 a[i])를 올바른 위치에 넣습니다.
-				// 위치
+				// temp(원본 a[i])를 올바른 위치에 넣음
 				array[j] = temp;
 			}
 		}
@@ -51,8 +49,7 @@ class ShellSort
 	// 구동 방법 : Driver method
 	public static void main(String args[])
 	{
-		int array[] = { 7
-				, 3, 5, 2, 6, 1, 4 };
+		int array[] = { 7, 3, 5, 2, 6, 1, 4 };
 		System.out.println("shell sorting 전");
 		printArray(array);
 
